6b65e82624d9df367e69476f7c1c79ff4e08eb6e
effus <effusps@gmail.com>
2023-12-23 18:28:18 +0300

    Updates

--- _nuxt/index.ad28ac27.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,a}from"./client-only.34e6bda4.js";import{g as _,h as s,o as c,i as l,w as e,a as r,b as u,d as m}from"./entry.f1345268.js";const C=_({__name:"index",setup(p){return s(),(i,d)=>{const n=o,t=a;return c(),l(t,null,{default:e(()=>[r("main",null,[u(n,null,{default:e(()=>[m(" Choose a game ")]),_:1})])]),_:1})}}});export{C as default};
